a74f8547a908f247d5f14c0cbc706518e94f4818
LeanBow <theleanbow@gmail.com>
2023-03-10 17:38:14 +0545

    Added necessary files

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Article } from './article/Article'
+export {default as brand } from './brand/brand'
+export {default as cta } from './cta/CTA'
+export {default as Feature } from './feature/Feature'
+export {default as navbar } from './navbar/navbar'
