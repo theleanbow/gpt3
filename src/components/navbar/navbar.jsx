30b21050179fa3e7e0601aa956845b3f9e16e741
LeanBow <theleanbow@gmail.com>
2023-03-12 22:12:35 +0545

    Ui Improvements

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
+import logo from "../../assets/logo.svg";
+import { useState } from "react";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { toggleMenu, setToggleMenu } = useState(false);
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
-
 export default Navbar;
